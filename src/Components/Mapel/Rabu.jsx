4b63c7a500eac81534b72150672e75d9d31b4cfc
audya774 <muhammadkhairuddin2507@gmail.com>
2025-03-10 18:02:49 +0700

    Add files via upload

---
+++ src/Components/Mapel/Rabu.jsx
@@ -0,0 +1,33 @@
+const Rabu = () => {
+    return (
+        <>
+        	<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="600">
+					<div className="w-[50%] text-base  font-medium">Gema literasi (Baca Al-Quran)</div>
+					<div className="flex justify-center items-center text-sm">07.45-08.00</div>
+				</div>
+				<div className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60" data-aos="fade-up" data-aos-duration="600">
+					<div className="w-[50%] text-base  font-medium">Matematika (Peminatan)</div>
+					<div className="flex justify-center items-center text-sm">08.00-09.25</div>
+				</div>
+				<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="700">
+					<div className="w-[50%] text-base  font-medium">Ekonomi</div>
+					<div className="flex justify-center items-center text-sm">09.05-10.25</div>
+				</div>
+				<div className=" border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="800">
+					<div className="w-[50%] text-base  font-medium">Istirahat</div>
+					<div className="flex justify-center items-center text-sm">10.25-10.45</div>
+				</div>
+				<div className="border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="700">
+					<div className="w-[50%] text-base  font-medium">B.Inggris</div>
+					<div className="flex justify-center items-center text-sm">10.45-12.05</div>
+				</div>
+				<div className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60 border-b-2 border-white" data-aos="fade-up" data-aos-duration="900">
+					<div className="w-[50%] text-base  font-medium">Fisika</div>
+					<div className="flex justify-center items-center text-sm">12.05-13.25</div>
+				</div>
+			
+        </>
+    )
+} 
+
+export default Rabu;
