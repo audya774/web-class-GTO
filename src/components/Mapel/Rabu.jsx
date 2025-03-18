0bb3655958951a6c01d7521d9f43f059a046fb5c
Khairul2507__ <muhammadkhairuddin2507@gmail.com>
2025-03-19 01:11:16 +0700

    Update Rabu.jsx

--- src/components/Mapel/Rabu.jsx
+++ src/components/Mapel/Rabu.jsx
@@ -18,7 +18,7 @@ const Rabu = () => {
 					<div className="flex justify-center items-center text-sm">10.25-10.45</div>
 				</div>
 				<div className="border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="700">
-					<div className="w-[50%] text-base  font-medium">B.Inggris</div>
+					<div className="w-[50%] text-base  font-medium">B. Inggris</div>
 					<div className="flex justify-center items-center text-sm">10.45-12.05</div>
 				</div>
 				<div className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60 border-b-2 border-white" data-aos="fade-up" data-aos-duration="900">
