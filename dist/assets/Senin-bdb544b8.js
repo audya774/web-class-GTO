dd8c76d8062d4c3916f13e7865233ad09d684d76
Khairul2507__ <muhammadkhairuddin2507@gmail.com>
2025-03-19 21:11:24 +0700

    Update Senin-bdb544b8.js

--- dist/assets/Senin-bdb544b8.js
+++ dist/assets/Senin-bdb544b8.js
@@ -1,36 +1,58 @@
 import{j as e}from"./index-7d358027.js";const t=()=>e.jsxs(e.Fragment,{children:[
-    e.jsxs("div",{className:"border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3","data-aos":"fade-up","data-aos-duration":"600",children:[
-        e.jsx("div",{className:"w-[40%] text-base font-medium",children:"Upacara"}),
-        e.jsx("div",{className:"w-[30%] flex justify-center items-center text-sm",children:"07.45-08.25"}),
-        e.jsx("div",{className:"w-[30%] flex justify-center items-center text-sm",children:"-"})]}),
     
-    e.jsxs("div",{className:"flex justify-between py-[0.50rem] w-72 px-3 opacity-60","data-aos":"fade-up","data-aos-duration":"600",children:[
-        e.jsx("div",{className:"w-[40%] text-base font-medium",children:"B. Indonesia"}),
-        e.jsx("div",{className:"w-[30%] flex justify-center items-center text-sm",children:"08.25-09.45"}),
-        e.jsx("div",{className:"w-[30%] flex justify-center items-center text-sm",children:"Bu Emi"})]}),
+    // Kepala tabel
+    e.jsxs("div",{className:"border-t-2 border-b-2 border-white flex justify-between font-bold py-[0.50rem] w-72 px-3 bg-gray-800 text-white","data-aos":"fade-up","data-aos-duration":"500",children:[
+        e.jsx("div",{className:"w-[20%] text-center",children:"Jam"}),
+        e.jsx("div",{className:"w-[30%] text-center",children:"Pukul"}),
+        e.jsx("div",{className:"w-[30%] text-center",children:"Jadwal"}),
+        e.jsx("div",{className:"w-[20%] text-center",children:"Guru"})]}),
 
-    e.jsxs("div",{className:"border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3","data-aos":"fade-up","data-aos-duration":"700",children:[
-        e.jsx("div",{className:"w-[40%] text-base font-medium",children:"Biologi"}),
-        e.jsx("div",{className:"w-[30%] flex justify-center items-center text-sm",children:"09.45-10.25"}),
-        e.jsx("div",{className:"w-[30%] flex justify-center items-center text-sm",children:"Pak Sumar"})]}),
+    // Baris data 1
+    e.jsxs("div",{className:"border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3","data-aos":"fade-up","data-aos-duration":"600",children:[
+        e.jsx("div",{className:"w-[20%] text-center",children:"1"}),
+        e.jsx("div",{className:"w-[30%] text-center",children:"07.45-08.25"}),
+        e.jsx("div",{className:"w-[30%] text-center",children:"Upacara"}),
+        e.jsx("div",{className:"w-[20%] text-center",children:"-"})]}),
 
-    e.jsxs("div",{className:"border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3","data-aos":"fade-up","data-aos-duration":"800",children:[
-        e.jsx("div",{className:"w-[40%] text-base font-medium",children:"Istirahat"}),
-        e.jsx("div",{className:"w-[30%] flex justify-center items-center text-sm",children:"10.25-10.45"}),
-        e.jsx("div",{className:"w-[30%] flex justify-center items-center text-sm",children:"-"})]}),
+    // Baris data 2
+    e.jsxs("div",{className:"flex justify-between py-[0.50rem] w-72 px-3 opacity-60 border-b-2 border-white","data-aos":"fade-up","data-aos-duration":"600",children:[
+        e.jsx("div",{className:"w-[20%] text-center",children:"2"}),
+        e.jsx("div",{className:"w-[30%] text-center",children:"08.25-09.45"}),
+        e.jsx("div",{className:"w-[30%] text-center",children:"B. Indonesia"}),
+        e.jsx("div",{className:"w-[20%] text-center",children:"Bu Emi"})]}),
 
-    e.jsxs("div",{className:"flex justify-between py-[0.50rem] w-72 px-3 opacity-60","data-aos":"fade-up","data-aos-duration":"900",children:[
-        e.jsx("div",{className:"w-[40%] text-base font-medium",children:"Biologi"}),
-        e.jsx("div",{className:"w-[30%] flex justify-center items-center text-sm",children:"10.45-11.25"}),
-        e.jsx("div",{className:"w-[30%] flex justify-center items-center text-sm",children:"Pak Sumar"})]}),
+    // Baris data 3
+    e.jsxs("div",{className:"border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3","data-aos":"fade-up","data-aos-duration":"700",children:[
+        e.jsx("div",{className:"w-[20%] text-center",children:"3"}),
+        e.jsx("div",{className:"w-[30%] text-center",children:"09.45-10.25"}),
+        e.jsx("div",{className:"w-[30%] text-center",children:"Biologi"}),
+        e.jsx("div",{className:"w-[20%] text-center",children:"Pak Sumar"})]}),
 
-    e.jsxs("div",{className:"border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3","data-aos":"fade-up","data-aos-duration":"1000",children:[
-        e.jsx("div",{className:"w-[40%] text-base font-medium",children:"B. Inggris"}),
-        e.jsx("div",{className:"w-[30%] flex justify-center items-center text-sm",children:"11.25-12.45"}),
-        e.jsx("div",{className:"w-[30%] flex justify-center items-center text-sm",children:"Bu Diah"})]}),
+    // Baris data 4 (Istirahat)
+    e.jsxs("div",{className:"border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3 bg-gray-700 text-white","data-aos":"fade-up","data-aos-duration":"800",children:[
+        e.jsx("div",{className:"w-[20%] text-center",children:"-"}),
+        e.jsx("div",{className:"w-[30%] text-center",children:"10.25-10.45"}),
+        e.jsx("div",{className:"w-[30%] text-center",children:"Istirahat"}),
+        e.jsx("div",{className:"w-[20%] text-center",children:"-"})]}),
 
+    // Baris data 5
+    e.jsxs("div",{className:"border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3 opacity-60","data-aos":"fade-up","data-aos-duration":"900",children:[
+        e.jsx("div",{className:"w-[20%] text-center",children:"4"}),
+        e.jsx("div",{className:"w-[30%] text-center",children:"10.45-11.25"}),
+        e.jsx("div",{className:"w-[30%] text-center",children:"Biologi"}),
+        e.jsx("div",{className:"w-[20%] text-center",children:"Pak Sumar"})]}),
+
+    // Baris data 6
     e.jsxs("div",{className:"border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3","data-aos":"fade-up","data-aos-duration":"1000",children:[
-        e.jsx("div",{className:"w-[40%] text-base font-medium",children:"Mulok"}),
-        e.jsx("div",{className:"w-[30%] flex justify-center items-center text-sm",children:"12.45-14.05"}),
-        e.jsx("div",{className:"w-[30%] flex justify-center items-center text-sm",children:"Pak Agus"})]})]
+        e.jsx("div",{className:"w-[20%] text-center",children:"5"}),
+        e.jsx("div",{className:"w-[30%] text-center",children:"11.25-12.45"}),
+        e.jsx("div",{className:"w-[30%] text-center",children:"B. Inggris"}),
+        e.jsx("div",{className:"w-[20%] text-center",children:"Bu Diah"})]}),
+
+    // Baris data 7
+    e.jsxs("div",{className:"border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3","data-aos":"fade-up","data-aos-duration":"1000",children:[
+        e.jsx("div",{className:"w-[20%] text-center",children:"6"}),
+        e.jsx("div",{className:"w-[30%] text-center",children:"12.45-14.05"}),
+        e.jsx("div",{className:"w-[30%] text-center",children:"Mulok"}),
+        e.jsx("div",{className:"w-[20%] text-center",children:"Pak Agus"})]})]
 });export{t as default};
