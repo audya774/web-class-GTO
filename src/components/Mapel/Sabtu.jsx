a0536408a4bd9400a7855e1887e712f222576b66
Khairul2507__ <muhammadkhairuddin2507@gmail.com>
2025-03-19 15:14:02 +0700

    Add files via upload

---
+++ src/components/Mapel/Sabtu.jsx
@@ -0,0 +1,37 @@
+const Sabtu = () => {
+    return (
+        <>
+        	<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="600">
+					<div className="w-[50%] text-base  font-medium">Sabtu berih(Gotong Royong)</div>
+					<div className="flex justify-center items-center text-sm">07.45-08.00</div>
+				</div>
+				<div className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60" data-aos="fade-up" data-aos-duration="600">
+					<div className="w-[50%] text-base  font-medium">Seni Budaya</div>
+					<div className="flex justify-center items-center text-sm">08.00-09.05</div>
+				</div>
+				<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="700">
+					<div className="w-[50%] text-base  font-medium">Matematika (Peminatan)</div>
+					<div className="flex justify-center items-center text-sm">09.05-10.25</div>
+				</div>
+				<div className=" border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="800">
+					<div className="w-[50%] text-base  font-medium">Istirahat</div>
+					<div className="flex justify-center items-center text-sm">10.25-10.45</div>
+				</div>
+
+				<div className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60" data-aos="fade-up" data-aos-duration="900">
+					<div className="w-[50%] text-base  font-medium">Biologi</div>
+					<div className="flex justify-center items-center text-sm">10.45-12.05</div>
+				</div>
+				<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="1000">
+					<div className="w-[50%] text-base font-medium">Sejarah (Wajib)</div>
+					<div className="flex justify-center items-center text-sm">12.05-13.25</div>
+				</div>
+				<div className=" border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="1000">
+					<div className="w-[50%] text-base font-medium">Shalat Dzuhur Berjamaah</div>
+					<div className="flex justify-center items-center text-sm">13.25-14.05</div>
+				</div>
+        </>
+    )
+} 
+
+export default Sabtu;
