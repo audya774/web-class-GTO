654536d9a4c0ce2369a4872edb5d854584ce53d1
Khairul2507__ <muhammadkhairuddin2507@gmail.com>
2025-03-19 14:32:06 +0700

    Update Jumat.jsx

--- src/components/Mapel/Jumat.jsx
+++ src/components/Mapel/Jumat.jsx
@@ -25,7 +25,7 @@ const Jumat = () => {
 			<div
 				className="border-b-2 flex justify-between py-[0.50rem] w-72 px-3 opacity-60"
 				data-aos="fade-up"
-				data-aos-duration="600">
+				data-aos-duration="700">
 				<div className="w-[50%] text-base  font-medium">Metamatika (Wajib)</div>
 		 		<div className="flex justify-center items-center text
 		 		-sm">10.15-11.35</div>
