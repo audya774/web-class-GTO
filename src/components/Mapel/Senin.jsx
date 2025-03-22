b298a3779be0608dbb4db8c76165a79a5b275bab
Khairul2507__ <muhammadkhairuddin2507@gmail.com>
2025-03-22 13:02:55 +0700

    Update Senin.jsx

--- src/components/Mapel/Senin.jsx
+++ src/components/Mapel/Senin.jsx
@@ -9,7 +9,7 @@ const Senin = () => {
                 <div className="w-[50%] text-base font-medium">Bahasa Indonesia</div>
                 <div className="flex justify-center items-center text-sm">08.25-09.45</div>
             </div>
-            <div className="border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="800">
+            <div className="flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="800">
                 <div className="w-[50%] text-base font-medium">Kimia</div>
                 <div className="flex justify-center items-center text-sm">09.45-10.25</div>
             </div>
@@ -21,7 +21,7 @@ const Senin = () => {
                 <div className="w-[50%] text-base font-medium">Kimia</div>
                 <div className="flex justify-center items-center text-sm">10.45-11.25</div>
             </div>
-            <div className="border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="1100">
+            <div className="flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="1100">
                 <div className="w-[50%] text-base font-medium">Matematika (Wajib)</div>
                 <div className="flex justify-center items-center text-sm">11.25-12.45</div>
             </div>
