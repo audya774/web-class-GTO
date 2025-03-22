275dbf507f2555601dac5768f494f4fb5b68c67a
Khairul2507__ <muhammadkhairuddin2507@gmail.com>
2025-03-22 14:52:32 +0700

    Update Rabu.jsx

--- src/components/Mapel/Rabu.jsx
+++ src/components/Mapel/Rabu.jsx
@@ -1,7 +1,8 @@
 const Rabu = () => {
     return (
         <>
-            <div className="border-t-2 border-b-2 border-yellow-500 bg-yellow-500 text-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="600">
+            <div className="border-t-2 border-white"></div>
+            <div className="border-b-2 border-white bg-yellow-500 text-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="600">
                 <div className="w-[50%] text-base font-medium">Gema Literasi (Baca Al-Qur'an)</div>
                 <div className="flex justify-center items-center text-sm">07.45-08.00</div>
             </div>
@@ -13,10 +14,11 @@ const Rabu = () => {
                 <div className="w-[50%] text-base font-medium">Ekonomi</div>
                 <div className="flex justify-center items-center text-sm">09.05-10.25</div>
             </div>
-            <div className="border-t-2 border-b-2 border-blue-500 bg-blue-500 text-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="800">
+            <div className="border-t-2 border-b-2 border-white bg-blue-500 text-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="800">
                 <div className="w-[50%] text-base font-medium">Istirahat</div>
                 <div className="flex justify-center items-center text-sm">10.25-10.45</div>
             </div>
+            <div className="border-b-2 border-white"></div>
             <div className="border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="900">
                 <div className="w-[50%] text-base font-medium">Bahasa Inggris</div>
                 <div className="flex justify-center items-center text-sm">10.45-12.05</div>
