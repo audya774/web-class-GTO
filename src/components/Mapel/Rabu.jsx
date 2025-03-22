a2dc6f55f4cd8c27569568cb7460b983c94a5d1d
Khairul2507__ <muhammadkhairuddin2507@gmail.com>
2025-03-22 15:07:40 +0700

    Update Rabu.jsx

--- src/components/Mapel/Rabu.jsx
+++ src/components/Mapel/Rabu.jsx
@@ -10,7 +10,7 @@ const Rabu = () => {
                 <div className="w-[50%] text-base font-medium">Matematika (Peminatan)</div>
                 <div className="flex justify-center items-center text-sm">08.00-09.05</div>
             </div>
-            <div className="border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="700">
+            <div className="flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="700">
                 <div className="w-[50%] text-base font-medium">Ekonomi</div>
                 <div className="flex justify-center items-center text-sm">09.05-10.25</div>
             </div>
