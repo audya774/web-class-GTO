8978ed950ee1ac60d7f2437465a2dbdb36b56095
Khairul2507__ <muhammadkhairuddin2507@gmail.com>
2025-03-27 11:58:58 +0700

    Update GuruMapel.jsx

--- src/Pages/GuruMapel.jsx
+++ src/Pages/GuruMapel.jsx
@@ -3,7 +3,7 @@ import Navbar from "../components/Navbar";
 const guruList = [
     { nama: "Dewi Andriyani, S.Pd", mapel: "B. Indonesia" },
     { nama: "Rasimah, S.Pd", mapel: "Kimia" },
-    { nama: "Narmuli, S.Pd", mapel: "Matematika (Wajib)" },
+    { nama: "Narmuli, S.Pd", mapel: "Matematika" },
     { nama: "Hj. Samsidar, S.Pd", mapel: "PPKN" },
     { nama: "Jaswadi, S.Si", mapel: "Prakarya" },
     { nama: "Heria Minarti, S.Pd", mapel: "Matematika (Peminatan)" },
@@ -19,8 +19,8 @@ const guruList = [
 
 const GuruMapel = () => {
     return (
-        <>
-            <div className="text-white text-lg font-bold mb-4 mx-[10%] mt-10 lg:text-center lg:text-3xl lg:mb-16" id="GuruMapel">
+        <div className="flex flex-col items-center mt-10"> {/* Wrapper utama agar semua elemen di tengah */}
+            <div className="text-white text-xl font-bold mb-4 mx-[10%] lg:text-left lg:text-4xl lg:mb-16" id="GuruMapel">
                 Guru Mapel
             </div>
 
@@ -28,7 +28,9 @@ const GuruMapel = () => {
                 <div 
                     key={index} 
                     className={`border-white text-white flex justify-between py-2 w-72 px-3 
-                        ${index === 0 || index === guruList.length - 1 ? "border-t-2 border-b-2" : "border-b-2"}`}
+                        ${index === 0 ? "border-t-2 border-b-2" : ""}
+                        ${index === guruList.length - 2 ? "border-b" : "border-b-2"} 
+                        ${index === guruList.length - 1 ? "border-t-2 border-b-2" : ""}`}
                     data-aos="fade-up" 
                     data-aos-duration={600 + index * 100}
                 >
@@ -36,7 +38,7 @@ const GuruMapel = () => {
                     <div className="flex justify-center items-center text-sm">{guru.mapel}</div>
                 </div>
             ))}
-        </>
+        </div>
     );
 };
 
