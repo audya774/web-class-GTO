83ab2089dee7c65861483fc1a6148158981f59ea
Khairul2507__ <muhammadkhairuddin2507@gmail.com>
2025-03-31 14:23:48 +0700

    Update GuruMapel.jsx

--- src/Pages/GuruMapel.jsx
+++ src/Pages/GuruMapel.jsx
@@ -26,7 +26,7 @@ const GuruMapel = () => {
     }, []);
 
     return (
-        <div id="GuruMapel" className="flex flex-col items-center mb-32">
+        <div id="GuruMapel" className="flex flex-col items-center mb-32 bg-red-500">
             {/* Judul "Guru Mapel" */}
             <h2 
                 className="text-white mb-3 mx-[10%] self-start lg:mb-12 text-[1.5rem] font-medium font-[Poppins]"
