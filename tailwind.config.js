9fe6cb0c15953ce85c355b5ba59fc32b3b155950
Khairul2507__ <muhammadkhairuddin2507@gmail.com>
2025-07-08 16:08:20 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,30 @@
 /** @type {import('tailwindcss').Config} */
+import scrollbar from 'tailwind-scrollbar'
+
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+      colors: {
+        primary: 'var(--primary-color)',
+        secondary: 'var(--secondary-color)',
+        tertiary: 'var(--tertiary-color)',
+        quaternary: 'var(--quaternary-color)',
+        quinary: 'var(--quinary-color)',
+        senary: 'var(--senary-color)',
+        septenary: 'var(--septenary-color)',
+        bgdark: 'var(--bg)',
+        bglight: 'var(--bg1)',
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    scrollbar,
+  ],
 }
