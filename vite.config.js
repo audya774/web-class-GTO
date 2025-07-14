6181cc5164abf054e1827a0a7c3cf7f12bf49cce
Muhammad Khairuddin <muhammadkhairuddin2507@gmail.com>
2025-07-14 10:06:30 +0700

    Fix blank screen - update base path

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: './',
+  build: {
+    chunkSizeWarningLimit: 1500, // batas ukuran chunk dinaikkan jadi 1.5MB
+  },
 })
 
