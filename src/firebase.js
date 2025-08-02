733023e24008fea5fcee7ff97bb7c6af693a3dc3
Muhammad Khairuddin <muhammadkhairuddin2507@gmail.com>
2025-08-03 06:36:57 +0700

    update firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,12 @@
 // src/firebase.js
-import { initializeApp } from "firebase/app";
-import { getStorage } from "firebase/storage";
-import { getFirestore } from "firebase/firestore";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
-// Konfigurasi dari environment variables
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
+
+// Konfigurasi dari .env
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
@@ -12,13 +14,17 @@ const firebaseConfig = {
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Inisialisasi Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
-export const storage = getStorage(app);
+// Ekspor layanan Firebase
 export const db = getFirestore(app);
+export const storage = getStorage(app);
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
+export { analytics };
 
