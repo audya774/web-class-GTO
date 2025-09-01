2dd8ff2c9fa5e6cf7bc91c1a50efb2e331fb6cc1
Khairul2507__ <muhammadkhairuddin2507@gmail.com>
2025-09-01 09:20:18 +0700

    Update Schedule.jsx

--- src/Pages/Schedule.jsx
+++ src/Pages/Schedule.jsx
@@ -7,52 +7,59 @@ const Selasa = React.lazy(() => import("../components/Mapel/Selasa"))
 const Rabu = React.lazy(() => import("../components/Mapel/Rabu"))
 const Kamis = React.lazy(() => import("../components/Mapel/Kamis"))
 const Jumat = React.lazy(() => import("../components/Mapel/Jumat"))
+const Sabtu = React.lazy(() => import("../components/Mapel/Sabtu"))
 
 const Schedule = () => {
-    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-    const currentDay = daysOfWeek[new Date().getDay()]
+    const daysOfWeek = [
+        "Minggu",
+        "Senin",
+        "Selasa",
+        "Rabu",
+        "Kamis",
+        "Jumat",
+        "Sabtu",
+    ]
+    const todayIndex = new Date().getDay()
+    const currentDay = daysOfWeek[todayIndex]
 
     useEffect(() => {
         AOS.init()
         AOS.refresh()
     }, [])
 
-    let piketGroup = []
-
-    // Menentukan kelompok piket berdasarkan hari
-    piketGroup = [
-        ["Amel", "Helmalia", "Fadli I", "Shera", "Nicollas"],
-        ["Windi", "Lista", "Jeriko", "Pratiwi", "Damar"],
-        ["Putri", "Paulista", "Firdaus", "Attala", "Ardian"],
-        ["Cariska", "Nila", "Wahid", "Togi", "Annisa"],
-        ["Sultan", "Zaini", "Fajri", "Arif"],
-    ]
+    // Data piket by day (0 = Minggu, 1 = Senin, dst)
+    const piketByDay = {
+        0: [], // Minggu kosong
+        1: ["Ilsa Khusbah", "Mifta Adila", "Muhammad Khairuddin", "Muhammar Teja", "Nazla Fatimah"],
+        2: ["Badria", "Kiramul Misbah", "M. Rifki Nanda", "Syifa Ulnadya", "Uswatun Husna Ramadani"],
+        3: ["Maisi Ayustisi", "Marzawan", "Muhammad Syafriadi", "Nashirah Aliya Safitri", "Nur Asnah"],
+        4: ["Arisma Dewi", "Kiflan Alaiya", "T. Miswar Andrian Hanafiah", "Yuli Salma Dewi"],
+        5: ["Ahmad Zaini", "Arifki", "Dinia Fitri", "Heliyati Fitri", "Nuri Farija"],
+        6: ["Alfiza Rusfan", "Muhammad Azwa", "Nazira Husnia", "Riska Amelia Ramadani"],
+    }
 
     const dayComponents = [
-        null, // Kosongkan indeks 0
-        Senin,
-        Selasa,
-        Rabu,
-        Kamis,
-        Jumat,
+        null,   // 0 -> Minggu
+        Senin,  // 1
+        Selasa, // 2
+        Rabu,   // 3
+        Kamis,  // 4
+        Jumat,  // 5
+        Sabtu,  // 6
     ]
 
-    // Menampilkan komponen berdasarkan hari saat ini
-    const TodayComponent = dayComponents[new Date().getDay()]
-
-    // Menampilkan nama-nama piket sesuai dengan hari saat ini
-    const currentPiketNames = piketGroup[new Date().getDay() - 1]
-
-    console.log("Current Day:", currentDay)
-    console.log("Piket Group:", piketGroup)
-    console.log("Current Piket Names:", currentPiketNames)
+    const TodayComponent = dayComponents[todayIndex]
+    const currentPiketNames = piketByDay[todayIndex] || []
 
     return (
         <>
             {/* Jadwal Mapel */}
-            <div className="lg:flex lg:justify-center lg:gap-32 lg:mb-10 lg:mt-16 ">
+            <div className="lg:flex lg:justify-center lg:gap-32 lg:mb-10 lg:mt-16">
                 <div className="text-white flex flex-col justify-center items-center mt-8 md:mt-3 overflow-y-hidden">
-                    <div className="text-2xl font-medium mb-5" data-aos="fade-up" data-aos-duration="500">
+                    <div
+                        className="text-2xl font-medium mb-5"
+                        data-aos="fade-up"
+                        data-aos-duration="500">
                         {currentDay}
                     </div>
                     <div data-aos="fade-up" data-aos-duration="400">
@@ -79,7 +86,7 @@ const Schedule = () => {
                     currentPiketNames.map((piketName, index) => (
                         <div
                             key={index}
-                            className={` border-t-2 border-white flex justify-center py-[0.50rem] w-72 px-3 ${
+                            className={`border-t-2 border-white flex justify-center py-[0.50rem] w-72 px-3 ${
                                 index === currentPiketNames.length - 1 ? "border-b-2" : ""
                             }`}
                             data-aos="fade-up"
@@ -88,7 +95,7 @@ const Schedule = () => {
                         </div>
                     ))
                 ) : (
-                    <p className="opacity-50">Tidak Ada Jadwal Hari Ini</p>
+                    <p className="opacity-50">Tidak Ada Piket Hari Ini</p>
                 )}
             </div>
         </>
